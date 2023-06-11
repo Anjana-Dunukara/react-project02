d5ebf31b1f1bbd2c9c4985013f2651116ef3364a
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-11 23:44:56 +0530

    add simple validation logic to form submission

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,19 +1,49 @@
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 const AddUser = (props) => {
+  const [userName, setUserName] = useState("");
+  const [age, setAge] = useState("");
+
   const AddUserHandler = (event) => {
     event.preventDefault();
+    if (userName.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    if (+age < 1) {
+      return;
+    }
+    console.log(userName, age);
+    setUserName("");
+    setAge("");
+  };
+
+  const changeNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const changeAgeHandler = (event) => {
+    setAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={AddUserHandler}>
         <label htmlFor="username">User Name</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={userName}
+          onChange={changeNameHandler}
+        ></input>
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number"></input>
+        <input
+          id="age"
+          value={age}
+          type="number"
+          onChange={changeAgeHandler}
+        ></input>
         <Button />
       </form>
     </Card>
