0c9f0f49027a72aa33de4331497157bf2c6a5c67
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-12 10:18:47 +0530

    create usersList component

---
+++ src/Components/Users/UsersList.js
@@ -0,0 +1,18 @@
+import styles from "./UsersList.module.css";
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul className={styles.ul}>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} Years old.)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
